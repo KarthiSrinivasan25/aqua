bbb1668e7e076e1b1eccb09ec01456b155b902c1
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2023-12-14 14:11:00 +0530

    SK commit

--- aqua/aqua/settings.py
+++ aqua/aqua/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = ['https://aqua-production-cb98.up.railway.app/']
+
 
 
 
@@ -151,3 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://aqua-production-cb98.up.railway.app/']
